169a62d94627bc877cbb74e372713c6e7ec0ad4e
JC DESIGN <ismailjosim99@gmail.com>
2022-11-26 18:24:27 +0600

    add footer section

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import img from '../assets/old-book-center-logo.png'
 
 const Footer = () => {
     return (
-        <div>
-            <h3>Footer Section</h3>
-        </div>
+        <footer className="footer p-10 bg-base-200 text-base-content">
+            <div>
+                <img src={img} alt="logo" />
+                <p>Old Book Ltd.<br />Providing reliable Service since 2020</p>
+            </div>
+            <div>
+                <span className="footer-title">Services</span>
+                <button className="link link-hover">Branding</button>
+                <button className="link link-hover">Design</button>
+                <button className="link link-hover">Marketing</button>
+                <button className="link link-hover">Advertisement</button>
+            </div>
+            <div>
+                <span className="footer-title">Company</span>
+                <button className="link link-hover">About us</button>
+                <button className="link link-hover">Contact</button>
+                <button className="link link-hover">Jobs</button>
+                <button className="link link-hover">Press kit</button>
+            </div>
+            <div>
+                <span className="footer-title">Legal</span>
+                <button className="link link-hover">Terms of use</button>
+                <button className="link link-hover">Privacy policy</button>
+                <button className="link link-hover">Cookie policy</button>
+            </div>
+        </footer>
     );
 };
 
